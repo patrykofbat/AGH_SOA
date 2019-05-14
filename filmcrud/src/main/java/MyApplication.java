07f2d65f69bb198f1452ab6c3604f7391db99127
Patryk <patrykpapior1997@gmail.com>
2019-05-14 20:14:47 +0200

    asda

--- filmcrud/src/main/java/MyApplication.java
+++ filmcrud/src/main/java/MyApplication.java
@@ -1,3 +1,5 @@
+import io.swagger.jaxrs.config.BeanConfig;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
@@ -25,6 +27,14 @@ public class MyApplication extends Application {
     public Set<Object> getSingletons()
     {
         return singletons;
+    }
+
+    private void init() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setSchemes(new String[] {"http"});
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/jee-swagger/api");
     }
 
 
